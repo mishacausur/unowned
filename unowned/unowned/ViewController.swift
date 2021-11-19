bef3581a2265aaae49279bde73ae360e567bec11
Misha Causur <mishacausur@mail.ru>
2021-11-19 19:11:50 +0500

    initial commit

---
+++ unowned/unowned/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unowned
+//
+//  Created by Misha Causur on 19.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
